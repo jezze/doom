992f5e1451549ec5a595d95e60646f35bb620b13
Jens Nyberg <jens.nyberg@gmail.com>
2013-12-13 01:25:10 +0100

    more cleanups

--- p_spec.c
+++ p_spec.c
@@ -33,7 +33,45 @@ typedef struct
   char        endname[9];
   char        startname[9];
   int         speed;
-} __attribute__((packed)) animdef_t;
+} animdef_t;
+
+animdef_t		animdefs[] =
+{
+    {false,	"NUKAGE3",	"NUKAGE1",	8},
+    {false,	"FWATER4",	"FWATER1",	8},
+    {false,	"SWATER4",	"SWATER1", 	8},
+    {false,	"LAVA4",	"LAVA1",	8},
+    {false,	"BLOOD3",	"BLOOD1",	8},
+
+    // DOOM II flat animations.
+    {false,	"RROCK08",	"RROCK05",	8},		
+    {false,	"SLIME04",	"SLIME01",	8},
+    {false,	"SLIME08",	"SLIME05",	8},
+    {false,	"SLIME12",	"SLIME09",	8},
+
+    {true,	"BLODGR4",	"BLODGR1",	8},
+    {true,	"SLADRIP3",	"SLADRIP1",	8},
+
+    {true,	"BLODRIP4",	"BLODRIP1",	8},
+    {true,	"FIREWALL",	"FIREWALA",	8},
+    {true,	"GSTFONT3",	"GSTFONT1",	8},
+    {true,	"FIRELAVA",	"FIRELAV3",	8},
+    {true,	"FIREMAG3",	"FIREMAG1",	8},
+    {true,	"FIREBLU2",	"FIREBLU1",	8},
+    {true,	"ROCKRED3",	"ROCKRED1",	8},
+
+    {true,	"BFALL4",	"BFALL1",	8},
+    {true,	"SFALL4",	"SFALL1",	8},
+    {true,	"WFALL4",	"WFALL1",	8},
+    {true,	"DBRAIN4",	"DBRAIN1",	8},
+	
+    {-1}
+};
+
+
+
+
+
 
 #define MAXANIMS 32
 
@@ -47,12 +85,7 @@ static void P_SpawnPushers(void);
 void P_InitPicAnims (void)
 {
   int         i;
-  const animdef_t *animdefs;
-  int         lump = W_GetNumForName("ANIMATED");
 
-
-
-  animdefs = (const animdef_t *)W_CacheLumpNum(lump);
 
   lastanim = anims;
   for (i=0 ; animdefs[i].istexture != -1 ; i++)
@@ -95,7 +128,6 @@ void P_InitPicAnims (void)
     lastanim->speed = animdefs[i].speed;
     lastanim++;
   }
-  W_UnlockLumpNum(lump);
 }
 
 side_t* getSide
