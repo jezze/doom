af7f52b2d506197272e72a99b7a02d34354621f0
Jens Nyberg <jens.nyberg@gmail.com>
2014-03-23 14:56:16 +0100

    remove doomdata

--- p_mobj.h
+++ p_mobj.h
@@ -4,9 +4,21 @@
 #include "tables.h"
 #include "m_fixed.h"
 #include "d_think.h"
-#include "doomdata.h"
+#include "doomtype.h"
 #include "info.h"
 
+#define NF_SUBSECTOR                    0x8000
+#define NO_INDEX                        ((unsigned short)-1)
+#define ML_BLOCKING                     1
+#define ML_BLOCKMONSTERS                2
+#define ML_TWOSIDED                     4
+#define ML_DONTPEGTOP                   8
+#define ML_DONTPEGBOTTOM                16
+#define ML_SECRET                       32
+#define ML_SOUNDBLOCK                   64
+#define ML_DONTDRAW                     128
+#define ML_MAPPED                       256
+#define ML_PASSUSE                      512
 #define MF_SPECIAL      (uint_64_t)(0x0000000000000001)
 #define MF_SOLID        (uint_64_t)(0x0000000000000002)
 #define MF_SHOOTABLE    (uint_64_t)(0x0000000000000004)
@@ -43,9 +55,116 @@
 #define MF_BOUNCES         LONGLONG(0x0000000200000000)
 #define MF_FRIEND          LONGLONG(0x0000000400000000)
 
-enum {
-  MIF_FALLING = 1,
-  MIF_ARMED = 2
+enum
+{
+
+    ML_LABEL,
+    ML_THINGS,
+    ML_LINEDEFS,
+    ML_SIDEDEFS,
+    ML_VERTEXES,
+    ML_SEGS,
+    ML_SSECTORS,
+    ML_NODES,
+    ML_SECTORS,
+    ML_REJECT,
+    ML_BLOCKMAP
+
+};
+
+typedef struct
+{
+
+    short x,y;
+
+} __attribute__((packed)) mapvertex_t;
+
+typedef struct
+{
+
+    short textureoffset;
+    short rowoffset;
+    char toptexture[8];
+    char bottomtexture[8];
+    char midtexture[8];
+    short sector;
+
+} __attribute__((packed)) mapsidedef_t;
+
+typedef struct
+{
+
+    unsigned short v1;
+    unsigned short v2;
+    unsigned short flags;
+    short special;
+    short tag;
+    unsigned short sidenum[2];
+
+} __attribute__((packed)) maplinedef_t;
+
+typedef struct
+{
+
+    short floorheight;
+    short ceilingheight;
+    char floorpic[8];
+    char ceilingpic[8];
+    short lightlevel;
+    short special;
+    short tag;
+
+} __attribute__((packed)) mapsector_t;
+
+typedef struct
+{
+
+    unsigned short numsegs;
+    unsigned short firstseg;
+
+} __attribute__((packed)) mapsubsector_t;
+
+typedef struct
+{
+
+    unsigned short v1;
+    unsigned short v2;
+    short angle;
+    unsigned short linedef;
+    short side;
+    short offset;
+
+} __attribute__((packed)) mapseg_t;
+
+typedef struct
+{
+
+  short x;
+  short y;
+  short dx;
+  short dy;
+  short bbox[2][4];
+  unsigned short children[2];
+
+} __attribute__((packed)) mapnode_t;
+
+typedef struct
+{
+
+    short x;
+    short y;
+    short angle;
+    short type;
+    short options;
+
+} __attribute__((packed)) mapthing_t;
+
+enum
+{
+
+    MIF_FALLING = 1,
+    MIF_ARMED = 2
+
 };
 
 typedef struct mobj_s
@@ -117,18 +236,18 @@ typedef struct mobj_s
 extern int iquehead;
 extern int iquetail;
 
-mobj_t  *P_SpawnMobj(fixed_t x, fixed_t y, fixed_t z, mobjtype_t type);
-void    P_RemoveMobj(mobj_t *th);
+mobj_t *P_SpawnMobj(fixed_t x, fixed_t y, fixed_t z, mobjtype_t type);
+void P_RemoveMobj(mobj_t *th);
 boolean P_SetMobjState(mobj_t *mobj, statenum_t state);
-void    P_MobjThinker(mobj_t *mobj);
-void    P_SpawnPuff(fixed_t x, fixed_t y, fixed_t z);
-void    P_SpawnBlood(fixed_t x, fixed_t y, fixed_t z, int damage);
-mobj_t  *P_SpawnMissile(mobj_t *source, mobj_t *dest, mobjtype_t type);
-void    P_SpawnPlayerMissile(mobj_t *source, mobjtype_t type);
+void P_MobjThinker(mobj_t *mobj);
+void P_SpawnPuff(fixed_t x, fixed_t y, fixed_t z);
+void P_SpawnBlood(fixed_t x, fixed_t y, fixed_t z, int damage);
+mobj_t *P_SpawnMissile(mobj_t *source, mobj_t *dest, mobjtype_t type);
+void P_SpawnPlayerMissile(mobj_t *source, mobjtype_t type);
 boolean P_IsDoomnumAllowed(int doomnum);
-void    P_SpawnMapThing (const mapthing_t*  mthing);
-void    P_SpawnPlayer(int n, const mapthing_t *mthing);
-void    P_CheckMissileSpawn(mobj_t*);
-void    P_ExplodeMissile(mobj_t*);
+void P_SpawnMapThing (const mapthing_t*  mthing);
+void P_SpawnPlayer(int n, const mapthing_t *mthing);
+void P_CheckMissileSpawn(mobj_t*);
+void P_ExplodeMissile(mobj_t*);
 
 #endif
