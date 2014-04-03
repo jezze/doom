51aa6c5faa965e4497d6f26234019acd3cadd7a9
Jens Nyberg <jens.nyberg@gmail.com>
2014-04-03 21:50:53 +0200

    removed hu_lib

--- hu_stuff.c
+++ hu_stuff.c
@@ -1,7 +1,6 @@
 #include "d_think.h"
 #include "doomstat.h"
 #include "hu_stuff.h"
-#include "hu_lib.h"
 #include "i_video.h"
 #include "st_stuff.h"
 #include "w_wad.h"
@@ -258,6 +257,355 @@ static char hud_monsecstr[80];
 static boolean bsdown;
 static int bscounter;
 
+#define BG                              1
+#define FG                              0
+#define BASE_WIDTH                      320
+#define noterased                       viewwindowx
+
+extern int key_backspace;
+extern int key_enter;
+
+static void HU_clearTextLine(hu_textline_t* t)
+{
+
+    t->linelen = t->len = 0;
+    t->l[0] = 0;
+    t->needsupdate = true;
+
+}
+
+static void HU_initTextLine(hu_textline_t* t, int x, int y, const patchnum_t* f, int sc, int cm)
+{
+
+    t->x = x;
+    t->y = y;
+    t->f = f;
+    t->sc = sc;
+    t->cm = cm;
+
+    HU_clearTextLine(t);
+
+}
+
+static boolean HU_addCharToTextLine(hu_textline_t *t, char ch)
+{
+
+    if (t->linelen == HU_MAXLINELENGTH)
+    {
+
+        return false;
+
+    }
+
+    else
+    {
+
+        t->linelen++;
+
+        if (ch == '\n')
+            t->linelen = 0;
+
+        t->l[t->len++] = ch;
+        t->l[t->len] = 0;
+        t->needsupdate = 4;
+
+        return true;
+
+    }
+
+}
+
+static void HU_drawTextLine(hu_textline_t *l, boolean drawcursor)
+{
+
+    int i;
+    int w;
+    int x;
+    unsigned char c;
+    int oc = l->cm;
+    int y = l->y;
+
+    x = l->x;
+
+    for (i = 0; i < l->len; i++)
+    {
+
+        c = toupper(l->l[i]);
+
+        if (c == '\n')
+            x = 0, y += 8;
+
+        else if (c == '\t')
+            x = x - x % 80 + 80;
+
+        else if (c=='\x1b')
+        {
+
+            if (++i < l->len)
+                if (l->l[i] >= '0' && l->l[i] <= '9')
+                    l->cm = l->l[i] - '0';
+
+        }
+
+        else if (c != ' ' && c >= l->sc && c <= 127)
+        {
+
+            w = l->f[c - l->sc].width;
+
+            if (x+w > BASE_WIDTH)
+                break;
+
+            V_DrawNumPatch(x, y, FG, l->f[c - l->sc].lumpnum, l->cm, VPT_TRANS | VPT_STRETCH);
+
+            x += w;
+
+        }
+
+        else
+        {
+        
+            x += 4;
+
+            if (x >= BASE_WIDTH)
+                break;
+
+        }
+
+    }
+
+    l->cm = oc;
+
+    if (drawcursor && x + l->f['_' - l->sc].width <= BASE_WIDTH)
+        V_DrawNumPatch(x, y, FG, l->f['_' - l->sc].lumpnum, CR_DEFAULT, VPT_NONE | VPT_STRETCH);
+
+}
+
+static void HU_eraseTextLine(hu_textline_t* l)
+{
+
+    int lh;
+    int y;
+
+    if (viewwindowx && l->needsupdate)
+    {
+
+        lh = l->f[0].height + 1;
+
+        for (y = l->y; y < l->y + lh; y++)
+        {
+
+            if (y < viewwindowy || y >= viewwindowy + viewheight)
+            {
+
+                R_VideoErase(0, y, SCREENWIDTH);
+
+            }
+
+            else
+            {
+
+                R_VideoErase(0, y, viewwindowx);
+                R_VideoErase(viewwindowx + viewwidth, y, viewwindowx);
+
+            }
+
+        }
+
+    }
+
+    if (l->needsupdate)
+        l->needsupdate--;
+
+}
+
+static void HU_initSText(hu_stext_t *s, int x, int y, int h, const patchnum_t *font, int startchar, int cm, boolean *on)
+{
+
+    int i;
+
+    s->h = h;
+    s->on = on;
+    s->laston = true;
+    s->cl = 0;
+
+    for (i = 0; i < h; i++)
+        HU_initTextLine(&s->l[i], x, y - i * (font[0].height + 1), font, startchar, cm);
+
+}
+
+static void HU_addLineToSText(hu_stext_t *s)
+{
+
+    int i;
+
+    if (++s->cl == s->h)
+        s->cl = 0;
+
+    HU_clearTextLine(&s->l[s->cl]);
+
+    for (i = 0; i < s->h; i++)
+        s->l[i].needsupdate = 4;
+
+}
+
+static void HU_addMessageToSText(hu_stext_t *s, const char *prefix, const char *msg)
+{
+
+    HU_addLineToSText(s);
+
+    if (prefix)
+    {
+
+        while (*prefix)
+            HU_addCharToTextLine(&s->l[s->cl], *(prefix++));
+
+    }
+
+    while (*msg)
+        HU_addCharToTextLine(&s->l[s->cl], *(msg++));
+
+}
+
+static void HU_drawSText(hu_stext_t *s)
+{
+
+    int i, idx;
+    hu_textline_t *l;
+
+    if (!*s->on)
+        return;
+
+    for (i = 0; i < s->h; i++)
+    {
+
+        idx = s->cl - i;
+
+        if (idx < 0)
+            idx += s->h;
+
+        l = &s->l[idx];
+
+        HU_drawTextLine(l, false);
+
+    }
+
+}
+
+static void HU_eraseSText(hu_stext_t* s)
+{
+
+    int i;
+
+    for (i = 0; i < s->h; i++)
+    {
+
+        if (s->laston && !*s->on)
+            s->l[i].needsupdate = 4;
+
+        HU_eraseTextLine(&s->l[i]);
+
+    }
+
+    s->laston = *s->on;
+
+}
+
+static void HU_initMText(hu_mtext_t *m, int x, int y, int w, int h, const patchnum_t* font, int startchar, int cm, const patchnum_t* bgfont, boolean *on)
+{
+
+    int i;
+
+    m->nl = 0;
+    m->nr = 0;
+    m->cl = -1;
+    m->x = x;
+    m->y = y;
+    m->w = w;
+    m->h = h;
+    m->bg = bgfont;
+    m->on = on;
+
+    for (i = 0; i < HU_MAXMESSAGES; i++)
+        HU_initTextLine(&m->l[i], x, y + i * HU_REFRESHSPACING, font, startchar, cm);
+
+}
+
+static void HU_addLineToMText(hu_mtext_t* m)
+{
+
+    if (++m->cl == hud_msg_lines)
+        m->cl = 0;
+
+    HU_clearTextLine(&m->l[m->cl]);
+
+    if (m->nl<hud_msg_lines)
+        m->nl++;
+
+    m->l[m->cl].needsupdate = 4;
+
+}
+
+static void HU_addMessageToMText(hu_mtext_t* m, const char* prefix, const char* msg)
+{
+
+    HU_addLineToMText(m);
+
+    if (prefix)
+    {
+
+        while (*prefix)
+            HU_addCharToTextLine(&m->l[m->cl], *(prefix++));
+
+    }
+
+    while (*msg)
+        HU_addCharToTextLine(&m->l[m->cl], *(msg++));
+
+}
+
+static void HU_drawMText(hu_mtext_t* m)
+{
+
+    int i, idx;
+    hu_textline_t *l;
+
+    if (!*m->on)
+        return;
+
+    for (i = 0; i < m->nl; i++)
+    {
+
+        idx = m->cl - i;
+
+        if (idx < 0)
+            idx += m->nl;
+
+        l = &m->l[idx];
+        l->x = m->x;
+        l->y = m->y + i * HU_REFRESHSPACING;
+
+        HU_drawTextLine(l, false);
+
+    }
+
+}
+
+static void HU_eraseMText(hu_mtext_t* m)
+{
+
+    int i;
+
+    for (i = 0; i < m->nl; i++)
+    {
+
+        m->l[i].needsupdate = 4;
+
+        HU_eraseTextLine(&m->l[i]);
+
+    }
+
+}
+
 void HU_Init(void)
 {
 
@@ -358,22 +706,22 @@ void HU_Start(void)
     message_dontfuckwithme = false;
     message_nottobefuckedwith = false;
 
-    HUlib_initSText(&w_message, HU_MSGX, HU_MSGY, HU_MSGHEIGHT, hu_font, HU_FONTSTART, hudcolor_mesg, &message_on);
-    HUlib_initTextLine(&w_title, HU_TITLEX, HU_TITLEY, hu_font, HU_FONTSTART, hudcolor_titl);
-    HUlib_initTextLine(&w_health, HU_HEALTHX, HU_HEALTHY, hu_font2, HU_FONTSTART, CR_GREEN);
-    HUlib_initTextLine(&w_armor, HU_ARMORX, HU_ARMORY, hu_font2, HU_FONTSTART, CR_GREEN);
-    HUlib_initTextLine(&w_ammo, HU_AMMOX, HU_AMMOY, hu_font2, HU_FONTSTART, CR_GOLD);
-    HUlib_initTextLine(&w_weapon, HU_WEAPX, HU_WEAPY, hu_font2, HU_FONTSTART, CR_GRAY);
-    HUlib_initTextLine(&w_keys, HU_KEYSX, HU_KEYSY, hu_font2, HU_FONTSTART, CR_GRAY);
-    HUlib_initTextLine(&w_gkeys, HU_KEYSGX, HU_KEYSY, hu_fontk, HU_FONTSTART, CR_RED);
-    HUlib_initTextLine(&w_monsec, HU_MONSECX, HU_MONSECY, hu_font2, HU_FONTSTART, CR_GRAY);
+    HU_initSText(&w_message, HU_MSGX, HU_MSGY, HU_MSGHEIGHT, hu_font, HU_FONTSTART, hudcolor_mesg, &message_on);
+    HU_initTextLine(&w_title, HU_TITLEX, HU_TITLEY, hu_font, HU_FONTSTART, hudcolor_titl);
+    HU_initTextLine(&w_health, HU_HEALTHX, HU_HEALTHY, hu_font2, HU_FONTSTART, CR_GREEN);
+    HU_initTextLine(&w_armor, HU_ARMORX, HU_ARMORY, hu_font2, HU_FONTSTART, CR_GREEN);
+    HU_initTextLine(&w_ammo, HU_AMMOX, HU_AMMOY, hu_font2, HU_FONTSTART, CR_GOLD);
+    HU_initTextLine(&w_weapon, HU_WEAPX, HU_WEAPY, hu_font2, HU_FONTSTART, CR_GRAY);
+    HU_initTextLine(&w_keys, HU_KEYSX, HU_KEYSY, hu_font2, HU_FONTSTART, CR_GRAY);
+    HU_initTextLine(&w_gkeys, HU_KEYSGX, HU_KEYSY, hu_fontk, HU_FONTSTART, CR_RED);
+    HU_initTextLine(&w_monsec, HU_MONSECX, HU_MONSECY, hu_font2, HU_FONTSTART, CR_GRAY);
 
     if (hud_msg_lines > HU_MAXMESSAGES)
         hud_msg_lines = HU_MAXMESSAGES;
 
     message_list = hud_msg_lines > 1;
 
-    HUlib_initMText(&w_rtext, 0, 0, 320, (hud_msg_lines + 2) * HU_REFRESHSPACING, hu_font, HU_FONTSTART, hudcolor_list, hu_msgbg, &message_list);
+    HU_initMText(&w_rtext, 0, 0, 320, (hud_msg_lines + 2) * HU_REFRESHSPACING, hu_font, HU_FONTSTART, hudcolor_list, hu_msgbg, &message_list);
 
     if (gamestate == GS_LEVEL)
     {
@@ -406,59 +754,59 @@ void HU_Start(void)
     }
 
     while (*s)
-        HUlib_addCharToTextLine(&w_title, *(s++));
+        HU_addCharToTextLine(&w_title, *(s++));
 
-    HUlib_initTextLine(&w_coordx, HU_COORDX, HU_COORDX_Y, hu_font, HU_FONTSTART, hudcolor_xyco);
-    HUlib_initTextLine(&w_coordy, HU_COORDX, HU_COORDY_Y, hu_font, HU_FONTSTART, hudcolor_xyco);
-    HUlib_initTextLine(&w_coordz, HU_COORDX, HU_COORDZ_Y, hu_font, HU_FONTSTART, hudcolor_xyco);
+    HU_initTextLine(&w_coordx, HU_COORDX, HU_COORDX_Y, hu_font, HU_FONTSTART, hudcolor_xyco);
+    HU_initTextLine(&w_coordy, HU_COORDX, HU_COORDY_Y, hu_font, HU_FONTSTART, hudcolor_xyco);
+    HU_initTextLine(&w_coordz, HU_COORDX, HU_COORDZ_Y, hu_font, HU_FONTSTART, hudcolor_xyco);
     strcpy(hud_ammostr,"AMM ");
 
     s = hud_ammostr;
 
     while (*s)
-        HUlib_addCharToTextLine(&w_ammo, *(s++));
+        HU_addCharToTextLine(&w_ammo, *(s++));
 
     strcpy(hud_healthstr,"HEL ");
 
     s = hud_healthstr;
 
     while (*s)
-        HUlib_addCharToTextLine(&w_health, *(s++));
+        HU_addCharToTextLine(&w_health, *(s++));
 
     strcpy(hud_armorstr,"ARM ");
 
     s = hud_armorstr;
 
     while (*s)
-        HUlib_addCharToTextLine(&w_armor, *(s++));
+        HU_addCharToTextLine(&w_armor, *(s++));
 
     strcpy(hud_weapstr,"WEA ");
 
     s = hud_weapstr;
 
     while (*s)
-        HUlib_addCharToTextLine(&w_weapon, *(s++));
+        HU_addCharToTextLine(&w_weapon, *(s++));
 
     strcpy(hud_keysstr,"KEY ");
 
     s = hud_keysstr;
 
     while (*s)
-        HUlib_addCharToTextLine(&w_keys, *(s++));
+        HU_addCharToTextLine(&w_keys, *(s++));
 
     strcpy(hud_gkeysstr," ");
 
     s = hud_gkeysstr;
 
     while (*s)
-        HUlib_addCharToTextLine(&w_gkeys, *(s++));
+        HU_addCharToTextLine(&w_gkeys, *(s++));
 
     strcpy(hud_monsecstr,"STS ");
 
     s = hud_monsecstr;
 
     while (*s)
-        HUlib_addCharToTextLine(&w_monsec, *(s++));
+        HU_addCharToTextLine(&w_monsec, *(s++));
 
     headsupactive = true;
 
@@ -484,7 +832,7 @@ void HU_Drawer(void)
         if (doit)
         {
 
-            HUlib_clearTextLine(&w_ammo);
+            HU_clearTextLine(&w_ammo);
             strcpy(hud_ammostr,"AMM ");
 
             if (weaponinfo[plr->readyweapon].ammo == am_noammo)
@@ -550,11 +898,11 @@ void HU_Drawer(void)
             s = hud_ammostr;
 
             while (*s)
-                HUlib_addCharToTextLine(&w_ammo, *(s++));
+                HU_addCharToTextLine(&w_ammo, *(s++));
 
         }
 
-        HUlib_drawTextLine(&w_ammo, false);
+        HU_drawTextLine(&w_ammo, false);
 
         if (doit)
         {
@@ -562,7 +910,7 @@ void HU_Drawer(void)
             int health = plr->health;
             int healthbars = health > 100 ? 25 : health / 4;
 
-            HUlib_clearTextLine(&w_health);
+            HU_clearTextLine(&w_health);
             sprintf(healthstr, "%3d", health);
 
             for (i = 4; i < 4 + healthbars / 4;)
@@ -610,11 +958,11 @@ void HU_Drawer(void)
             s = hud_healthstr;
 
             while (*s)
-                HUlib_addCharToTextLine(&w_health, *(s++));
+                HU_addCharToTextLine(&w_health, *(s++));
 
         }
 
-        HUlib_drawTextLine(&w_health, false);
+        HU_drawTextLine(&w_health, false);
 
         if (doit)
         {
@@ -622,7 +970,7 @@ void HU_Drawer(void)
             int armor = plr->armorpoints;
             int armorbars = armor >100 ? 25 : armor / 4;
 
-            HUlib_clearTextLine(&w_armor);
+            HU_clearTextLine(&w_armor);
             sprintf(armorstr,"%3d",armor);
 
             for (i = 4; i < 4 + armorbars / 4;)
@@ -670,11 +1018,11 @@ void HU_Drawer(void)
             s = hud_armorstr;
 
             while (*s)
-                HUlib_addCharToTextLine(&w_armor, *(s++));
+                HU_addCharToTextLine(&w_armor, *(s++));
 
         }
 
-        HUlib_drawTextLine(&w_armor, false);
+        HU_drawTextLine(&w_armor, false);
 
         if (doit)
         {
@@ -682,7 +1030,7 @@ void HU_Drawer(void)
             int w;
             int ammo, fullammo, ammopct;
 
-            HUlib_clearTextLine(&w_weapon);
+            HU_clearTextLine(&w_weapon);
             i = 4;
             hud_weapstr[i] = '\0';
 
@@ -744,11 +1092,11 @@ void HU_Drawer(void)
             s = hud_weapstr;
 
             while (*s)
-                HUlib_addCharToTextLine(&w_weapon, *(s++));
+                HU_addCharToTextLine(&w_weapon, *(s++));
 
         }
 
-        HUlib_drawTextLine(&w_weapon, false);
+        HU_drawTextLine(&w_weapon, false);
 
         if (doit)
         {
@@ -775,37 +1123,37 @@ void HU_Drawer(void)
 
         }
 
-        HUlib_clearTextLine(&w_keys);
-        HUlib_clearTextLine(&w_gkeys);
+        HU_clearTextLine(&w_keys);
+        HU_clearTextLine(&w_gkeys);
 
         s = hud_keysstr;
 
         while (*s)
-            HUlib_addCharToTextLine(&w_keys, *(s++));
+            HU_addCharToTextLine(&w_keys, *(s++));
 
-        HUlib_drawTextLine(&w_keys, false);
+        HU_drawTextLine(&w_keys, false);
 
         s = hud_gkeysstr;
 
         while (*s)
-            HUlib_addCharToTextLine(&w_gkeys, *(s++));
+            HU_addCharToTextLine(&w_gkeys, *(s++));
 
-        HUlib_drawTextLine(&w_gkeys, false);
+        HU_drawTextLine(&w_gkeys, false);
 
         if (doit)
         {
 
-            HUlib_clearTextLine(&w_monsec);
+            HU_clearTextLine(&w_monsec);
             sprintf(hud_monsecstr, "STS \x1b\x36K \x1b\x33%d \x1b\x36M \x1b\x33%d \x1b\x37I \x1b\x33%d/%d \x1b\x35S \x1b\x33%d/%d", plr->killcount, totallive, plr->itemcount, totalitems, plr->secretcount, totalsecret);
 
             s = hud_monsecstr;
 
             while (*s)
-                HUlib_addCharToTextLine(&w_monsec, *(s++));
+                HU_addCharToTextLine(&w_monsec, *(s++));
 
         }
 
-        HUlib_drawTextLine(&w_monsec, false);
+        HU_drawTextLine(&w_monsec, false);
 
     }
 
@@ -815,10 +1163,10 @@ void HU_Drawer(void)
         message_list = false;
 
     if (!message_list)
-        HUlib_drawSText(&w_message);
+        HU_drawSText(&w_message);
 
     if (hud_msg_lines > 1 && message_list)
-        HUlib_drawMText(&w_rtext);
+        HU_drawMText(&w_rtext);
 
 }
 
@@ -826,11 +1174,11 @@ void HU_Erase(void)
 {
 
     if (!message_list)
-        HUlib_eraseSText(&w_message);
+        HU_eraseSText(&w_message);
     else
-        HUlib_eraseMText(&w_rtext);
+        HU_eraseMText(&w_rtext);
 
-    HUlib_eraseTextLine(&w_title);
+    HU_eraseTextLine(&w_title);
 
 }
 
@@ -855,8 +1203,8 @@ void HU_Ticker(void)
     if ((plr->message && !message_nottobefuckedwith) || (plr->message && message_dontfuckwithme))
     {
 
-        HUlib_addMessageToSText(&w_message, 0, plr->message);
-        HUlib_addMessageToMText(&w_rtext, 0, plr->message);
+        HU_addMessageToSText(&w_message, 0, plr->message);
+        HU_addMessageToMText(&w_rtext, 0, plr->message);
 
         plr->message = 0;
         message_on = true;
--- hu_stuff.h
+++ hu_stuff.h
@@ -2,6 +2,8 @@
 #define __HU_STUFF_H__
 
 #include "d_event.h"
+#include "r_defs.h"
+#include "v_video.h"
 
 #define HU_FONTSTART                    '!'
 #define HU_FONTEND                      0x7f
@@ -12,6 +14,61 @@
 #define HU_MSGWIDTH                     64
 #define HU_MSGHEIGHT                    1
 #define HU_MSGTIMEOUT                   (4 * TICRATE)
+#define HU_MAXLINES                     4
+#define HU_MAXLINELENGTH                80
+#define HU_REFRESHSPACING               8
+#define HU_MAXMESSAGES                  16
+#define MAXLINES                        25
+
+typedef struct
+{
+
+    int x;
+    int y;
+    const patchnum_t *f;
+    int sc;
+    int cm;
+    int linelen;
+    char l[HU_MAXLINELENGTH * MAXLINES + 1];
+    int len;
+    int needsupdate;
+
+} hu_textline_t;
+
+typedef struct
+{
+
+    hu_textline_t l[HU_MAXLINES];
+    int h;
+    int cl;
+    boolean *on;
+    boolean laston;
+
+} hu_stext_t;
+
+typedef struct
+{
+
+    hu_textline_t l[HU_MAXMESSAGES];
+    int nl;
+    int nr;
+    int cl;
+    int x,y,w,h;
+    const patchnum_t *bg;
+    boolean *on;
+    boolean laston;
+
+} hu_mtext_t;
+
+typedef struct
+{
+
+    hu_textline_t l;
+    int lm;
+    boolean *on;
+    boolean laston;
+
+} hu_itext_t;
 
 void HU_Init(void);
 void HU_Start(void);
