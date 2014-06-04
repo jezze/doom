4b42f5533b0f908fada27690d156b393dbebcd03
Jens Nyberg <jens.nyberg@gmail.com>
2014-06-05 01:37:02 +0200

    removed cast

--- f_finale.c
+++ f_finale.c
@@ -18,15 +18,6 @@
 #define TEXTWAIT                        250
 #define NEWTEXTSPEED                    0.01f
 #define NEWTEXTWAIT                     1000
-#define MAX_CASTORDER                   18
-
-typedef struct
-{
-
-    const char *name;
-    mobjtype_t   type;
-
-} castinfo_t;
 
 static int finalestage;
 static int finalecount;
@@ -43,56 +34,10 @@ static const char *bgflat20 = "RROCK07";
 static const char *bgflat30 = "RROCK17";
 static const char *bgflat15 = "RROCK13";
 static const char *bgflat31 = "RROCK19";
-static const char *bgcastcall = "BOSSBACK";
-static int castnum;
-static int casttics;
-static state_t *caststate;
-static boolean castdeath;
-static int castframes;
-static int castonmelee;
-static boolean castattacking;
-
-static const castinfo_t castorder[] = {
-    {CC_ZOMBIE, MT_POSSESSED},
-    {CC_SHOTGUN, MT_SHOTGUY},
-    {CC_HEAVY, MT_CHAINGUY},
-    {CC_IMP, MT_TROOP},
-    {CC_DEMON, MT_SERGEANT},
-    {CC_LOST, MT_SKULL},
-    {CC_CACO, MT_HEAD},
-    {CC_HELL, MT_KNIGHT},
-    {CC_BARON, MT_BRUISER},
-    {CC_ARACH, MT_BABY},
-    {CC_PAIN, MT_PAIN},
-    {CC_REVEN, MT_UNDEAD},
-    {CC_MANCU, MT_FATSO},
-    {CC_ARCH, MT_VILE},
-    {CC_SPIDER, MT_SPIDER},
-    {CC_CYBER, MT_CYBORG},
-    {CC_HERO, MT_PLAYER},
-    {NULL, 0}
-};
-
 static const char pfub2[] = {"PFUB2"};
 static const char pfub1[] = {"PFUB1"};
 
 extern patchnum_t hu_font[HU_FONTSIZE];
-
-static void F_StartCast(void)
-{
-
-    castnum = 0;
-    caststate = &states[mobjinfo[castorder[castnum].type].seestate];
-    casttics = caststate->tics;
-    castdeath = false;
-    finalestage = 2;
-    castframes = 0;
-    castonmelee = 0;
-    castattacking = false;
-
-    S_ChangeMusic(mus_evil, true);
-
-}
 
 void F_StartFinale(void)
 {
@@ -208,33 +153,8 @@ void F_StartFinale(void)
 
 }
 
-static boolean F_CastResponder(event_t* ev)
-{
-
-    if (ev->type != ev_keydown)
-        return false;
-
-    if (castdeath)
-        return true;
-
-    castdeath = true;
-    caststate = &states[mobjinfo[castorder[castnum].type].deathstate];
-    casttics = caststate->tics;
-    castframes = 0;
-    castattacking = false;
-
-    if (mobjinfo[castorder[castnum].type].deathsound)
-        S_StartSound(NULL, mobjinfo[castorder[castnum].type].deathsound);
-
-    return true;
-
-}
-
 boolean F_Responder(event_t *event)
 {
-
-    if (finalestage == 2)
-        return F_CastResponder(event);
 
     return false;
 
@@ -307,126 +227,6 @@ static void F_TextWrite(void)
 
 }
 
-static void F_CastTicker(void)
-{
-
-    int st;
-    int sfx;
-
-    if (--casttics > 0)
-        return;
-
-    if (caststate->tics == -1 || caststate->nextstate == S_NULL)
-    {
-
-        castnum++;
-        castdeath = false;
-
-        if (castorder[castnum].name == NULL)
-            castnum = 0;
-            
-        if (mobjinfo[castorder[castnum].type].seesound)
-            S_StartSound (NULL, mobjinfo[castorder[castnum].type].seesound);
-
-        caststate = &states[mobjinfo[castorder[castnum].type].seestate];
-        castframes = 0;
-
-    }
-
-    else
-    {
-
-        if (caststate == &states[S_PLAY_ATK1])
-            goto stopattack;
-
-        st = caststate->nextstate;
-        caststate = &states[st];
-        castframes++;
-
-        switch (st)
-        {
-
-        case S_PLAY_ATK1:     sfx = sfx_dshtgn; break;
-        case S_POSS_ATK2:     sfx = sfx_pistol; break;
-        case S_SPOS_ATK2:     sfx = sfx_shotgn; break;
-        case S_VILE_ATK2:     sfx = sfx_vilatk; break;
-        case S_SKEL_FIST2:    sfx = sfx_skeswg; break;
-        case S_SKEL_FIST4:    sfx = sfx_skepch; break;
-        case S_SKEL_MISS2:    sfx = sfx_skeatk; break;
-        case S_FATT_ATK8:
-        case S_FATT_ATK5:
-        case S_FATT_ATK2:     sfx = sfx_firsht; break;
-        case S_CPOS_ATK2:
-        case S_CPOS_ATK3:
-        case S_CPOS_ATK4:     sfx = sfx_shotgn; break;
-        case S_TROO_ATK3:     sfx = sfx_claw; break;
-        case S_SARG_ATK2:     sfx = sfx_sgtatk; break;
-        case S_BOSS_ATK2:
-        case S_BOS2_ATK2:
-        case S_HEAD_ATK2:     sfx = sfx_firsht; break;
-        case S_SKULL_ATK2:    sfx = sfx_sklatk; break;
-        case S_SPID_ATK2:
-        case S_SPID_ATK3:     sfx = sfx_shotgn; break;
-        case S_BSPI_ATK2:     sfx = sfx_plasma; break;
-        case S_CYBER_ATK2:
-        case S_CYBER_ATK4:
-        case S_CYBER_ATK6:    sfx = sfx_rlaunc; break;
-        case S_PAIN_ATK3:     sfx = sfx_sklatk; break;
-        default: sfx = 0; break;
-
-        }
-
-        if (sfx)
-            S_StartSound (NULL, sfx);
-
-    }
-
-    if (castframes == 12)
-    {
-
-        castattacking = true;
-
-        if (castonmelee)
-            caststate = &states[mobjinfo[castorder[castnum].type].meleestate];
-        else
-            caststate = &states[mobjinfo[castorder[castnum].type].missilestate];
-
-        castonmelee ^= 1;
-
-        if (caststate == &states[S_NULL])
-        {
-
-            if (castonmelee)
-                caststate = &states[mobjinfo[castorder[castnum].type].meleestate];
-            else
-                caststate = &states[mobjinfo[castorder[castnum].type].missilestate];
-
-        }
-
-    }
-
-    if (castattacking)
-    {
-
-        if (castframes == 24 || caststate == &states[mobjinfo[castorder[castnum].type].seestate])
-        {
-
-        stopattack:
-            castattacking = false;
-            castframes = 0;
-            caststate = &states[mobjinfo[castorder[castnum].type].seestate];
-
-        }
-
-    }
-
-    casttics = caststate->tics;
-
-    if (casttics == -1)
-        casttics = 15;
-
-}
-
 void F_Ticker(void)
 {
 
@@ -448,9 +248,6 @@ void F_Ticker(void)
     }
 
     finalecount++;
-
-    if (finalestage == 2)
-        F_CastTicker();
 
     if (!finalestage)
     {
@@ -475,9 +272,6 @@ void F_Ticker(void)
             {
 
             next_level:
-                if (gamemap == 30)
-                    F_StartCast();
-                else
                     gameaction = ga_worlddone;
 
             }
@@ -485,91 +279,6 @@ void F_Ticker(void)
         }
 
     }
-
-}
-
-static void F_CastPrint(const char *text)
-{
-
-    const char *ch = text;
-    int c;
-    int cx;
-    int w;
-    int width = 0;
-
-    while (ch)
-    {
-
-        c = *ch++;
-
-        if (!c)
-            break;
-
-        c = toupper(c) - HU_FONTSTART;
-
-        if (c < 0 || c> HU_FONTSIZE)
-        {
-
-            width += 4;
-
-            continue;
-
-        }
-
-        w = hu_font[c].width;
-        width += w;
-
-    }
-
-    cx = 160 - width / 2;
-    ch = text;
-
-    while (ch)
-    {
-
-        c = *ch++;
-
-        if (!c)
-            break;
-
-        c = toupper(c) - HU_FONTSTART;
-
-        if (c < 0 || c > HU_FONTSIZE)
-        {
-
-            cx += 4;
-
-            continue;
-
-        }
-
-        w = hu_font[c].width;
-
-        V_DrawNumPatch(cx, 180, 0, hu_font[c].lumpnum, CR_DEFAULT, VPT_STRETCH);
-
-        cx += w;
-
-    }
-
-}
-
-static void F_CastDrawer(void)
-{
-
-    spritedef_t *sprdef;
-    spriteframe_t *sprframe;
-    int lump;
-    boolean flip;
-
-    V_DrawNamePatch(0,0,0, bgcastcall, CR_DEFAULT, VPT_STRETCH);
-    F_CastPrint(castorder[castnum].name);
-
-    sprdef = &sprites[caststate->sprite];
-    sprframe = &sprdef->spriteframes[caststate->frame & FF_FRAMEMASK];
-    lump = sprframe->lump[0];
-    flip = (boolean)sprframe->flip[0];
-
-    V_DrawNumPatch(160, 170, 0, lump+firstspritelump, CR_DEFAULT, VPT_STRETCH | (flip ? VPT_FLIP : 0));
 
 }
 
@@ -642,15 +351,6 @@ static void F_BunnyScroll(void)
 
 void F_Drawer(void)
 {
-
-    if (finalestage == 2)
-    {
-
-        F_CastDrawer();
-
-        return;
-
-    }
 
     if (!finalestage)
         F_TextWrite();
